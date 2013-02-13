37c258170ea5048ee1010f9f8bb17703cae021ad
Rustam Ibragimov <Rustam.Ibragimov@softline.ru>
2013-02-13 15:16:35 +0400

    блок хит продаж с текстом

--- public/js/auto.js
+++ public/js/auto.js
@@ -3,26 +3,40 @@ function hit_right() {
 	img_all = $('.hit_prod img');
 
 	img_shown = $('.hit_prod img[id='+hit_img_id+']');
+	text_shown = $('.hit_prod div[id='+hit_img_id+']');
+
 	img_shown.animate({opacity:0});
+	text_shown.animate({opacity:0});
+
 	if(hit_img_id == img_all.length-1) {
 		hit_img_id = 0;
 	} else {
 		hit_img_id = hit_img_id+1;
 	}
+
 	img_shown = $('.hit_prod img[id='+hit_img_id+']');
+	text_shown = $('.hit_prod div[id='+hit_img_id+']');
 	img_shown.animate({opacity:1});
+	text_shown.animate({opacity:1});
 }
 
 function hit_left() {
 	img_all = $('.hit_prod img');
 
 	img_shown = $('.hit_prod img[id='+hit_img_id+']');
+	text_shown = $('.hit_prod div[id='+hit_img_id+']');
+
 	img_shown.animate({opacity:0});
+	text_shown.animate({opacity:0});
+
 	if(hit_img_id == 0) {
 		hit_img_id =  img_all.length - 1;
 	} else {
 		hit_img_id = hit_img_id-1;
 	}
+
 	img_shown = $('.hit_prod img[id='+hit_img_id+']');
+	text_shown = $('.hit_prod div[id='+hit_img_id+']');
 	img_shown.animate({opacity:1});
+	text_shown.animate({opacity:1});
 }
