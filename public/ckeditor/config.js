a52e9927d8dcbfd91aaf857dcf1e8ee37e41d7e8
Rustam Ibragimov <Rustam.Ibragimov@softline.ru>
2013-02-25 12:26:06 +0400

    функционал загрузки картинок

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -10,24 +10,8 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.resize_enabled = false;
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+
 
 	// Remove some buttons, provided by the standard plugins, which we don't
 	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
 };
---
+++ public/ckeditor_uploader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rustam
+ * Date: 25.02.13
+ * Time: 11:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+move_uploaded_file($_FILES['upload']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/images/'.$_FILES['upload']['name']);?>
+<script type="text/javascript">
+	window.parent.document.getElementById('cke_69_textInput').value='/images/<?=$_FILES['upload']['name']?>';
+	window.parent.document.getElementById('cke_info_106').click();
+</script>
